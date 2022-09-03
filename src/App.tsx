6005c967475555f6e02c06302b20db6686a1032e
Joao Vitor Brito <joaovbrito123@gmail.com>
2022-09-03 20:20:02 -0300

    ESlint

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { ThemeProvider } from "styled-components"
-import { Button } from "./components/Button"
-import { GlobalStyle } from "./styles/global"
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './components/Router'
+import { GlobalStyle } from './styles/global'
 
-import { defaultTheme } from "./styles/themes/default"
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
